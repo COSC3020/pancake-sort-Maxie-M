f6b48a01d4e47fa26e0a0477aa059dfda181b43c
Maxie <144727967+Maxie-M@users.noreply.github.com>
2024-11-07 22:56:24 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,8 +1,41 @@
-function flip(array, n) {
+//code.js
+//Maxie Machado 
+//pancake sorting 
+
+function flip(array, n) 
+{
+    let left = 0;
+    let right = n -1;
+
+    while (left < right) 
+    {
+        [array[left], array[right]] = [array[right], array[left]];
+        left++;
+        right--;
+    }
     return array;
 }
 
 // Use only flip() here to manipulate the array
-function pancakeSort(array) {
+function pancakeSort(array) 
+{
+    for (let size = array.length; size > 1; size--)
+    {
+        let maxIndex = 0;
+
+        for (let i = 1; i < size; i++)
+        {
+            if (array[i] > array[maxIndex])
+            {
+                maxIndex = i;
+            }
+        }
+
+        if (maxIndex !== size - 1) 
+        {
+            flip(array, maxIndex + 1);
+            flip(array, size);
+        }
+    }
     return array;
 }
